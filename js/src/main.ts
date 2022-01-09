5e8da9d09c2e8ff238966df9ee523fd5c4318f1b
David Castellanos <davcastellanoslarios@gmail.com>
2022-01-09 16:41:56 -0600

    some rules from ts

--- js/src/main.ts
+++ js/src/main.ts
@@ -10,3 +10,6 @@ let hello2 = "word2";
 //in this case Let variable will allowe us to set a new value in it, we won't get an error, don't worry
 hello2 = "thisIsWord";
 // hello2 : "string" = "";
+// in type script we are allow to retype into a variable that contains the same type, that means if we are using a variable with type string WE MUST use string value, if not we will get an error for sure
+// ❌ => hello2 = true;
+//type 'true' is not assignable to type 'string'
