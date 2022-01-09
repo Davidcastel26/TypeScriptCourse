a2675ad47411c8839ab8eccba980af9665040c2e
David Castellanos <davcastellanoslarios@gmail.com>
2022-01-09 16:36:30 -0600

    renamed the file js

--- js/dist/main.js
+++ js/dist/main.js
@@ -1,7 +1,10 @@
 //const works as a real const
-const hello1 = "word";
+var hello1 = "word";
 //since we are defining the variable as a default value, if we will define it as a new value we will get an error for sure
 // hello : '23';
 // ❌ => hello = "foo"
-
 console.log(hello1);
+//Variable Let
+var hello2 = "word2";
+//in this case Let variable will allowe us to set a new value in it, we won't get an error, don't worry
+hello2 = "thisIsWord";
--- js/src/main.ts
+++ js/src/main.ts
@@ -1,6 +1,11 @@
 //const works as a real const
-var hello1 = "word";
+const hello1 = "word";
 //since we are defining the variable as a default value, if we will define it as a new value we will get an error for sure
 // hello : '23';
 // ❌ => hello = "foo"
 console.log(hello1);
+
+//Variable Let
+let hello2 = "word2";
+//in this case Let variable will allowe us to set a new value in it, we won't get an error, don't worry
+hello2 = "thisIsWord";
